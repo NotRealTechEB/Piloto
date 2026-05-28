12f0786645801ef93c63cadc46b8266ce8c9e2dd
Esalamancaa <esteban.salamanca.aravena@gmail.com>
2026-05-27 21:38:39 -0400

    Cambios validación DTO

--- dgac.piloto/dgac.piloto/src/main/java/cl/dgac/piloto/dto/UpdatePilotoRequest.java
+++ dgac.piloto/dgac.piloto/src/main/java/cl/dgac/piloto/dto/UpdatePilotoRequest.java
@@ -1,8 +1,8 @@
 package cl.dgac.piloto.dto;
 
 import jakarta.validation.constraints.Digits;
-import jakarta.validation.constraints.NegativeOrZero;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 public record UpdatePilotoRequest(
@@ -14,7 +14,7 @@ public record UpdatePilotoRequest(
     @NotBlank(message = "Debe ingresar el apellido materno del piloto") String apMaternoPiloto,
 
     //Ingreso de rut
-    @NegativeOrZero(message = "El rut no puede ser negativo o igual a cero") 
+    @Positive(message = "El rut no puede ser negativo o igual a cero") 
     @Digits(integer = 9, fraction = 0, message = "El rut no puede tener más de 9 dígitos") int rutPiloto
     )
     {
